c1b8c9cddd79ba41e375811f846dae2af2bda9c3
Denys <bakhonin1@gmail.com>
2023-06-01 19:15:53 +0200

    task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,9 @@
 const borderInputEl = document.querySelector('#validation-input');
 
 borderInputEl.addEventListener('blur', (event) => {
-    (event.currentTarget.value.trim().length === +borderInputEl.getAttribute('data-length')) ? 
-    borderInputEl.classList.add('valid') && borderInputEl.classList.remove('invalid') :
-     borderInputEl.classList.add('invalid') && borderInputEl.classList.remove('valid');
+    if (event.currentTarget.value.trim().length === +borderInputEl.getAttribute('data-length')){
+    borderInputEl.classList.add('valid') && borderInputEl.classList.remove('invalid')
+}else {
+     borderInputEl.classList.add('invalid') && borderInputEl.classList.remove('valid');}
 }
 );
